6affc3e66c7dcc826d6ead1ba450eb5c3c964221
dldks321 <80047618+dldks321@users.noreply.github.com>
2022-05-31 22:36:12 +0900

    [refactor] Implement Builder Pattern to Team.java
    Implement Builder Pattern to Team.java
    for a more intuitive understanding

--- core/src/mindustry/game/Team.java
+++ core/src/mindustry/game/Team.java
@@ -28,20 +28,59 @@ public class Team implements Comparable<Team>{
     public static final Team[] baseTeams = new Team[6];
 
     public final static Team
-        derelict = new Team(0, "derelict", Color.valueOf("4d4e58")),
-        sharded = new Team(1, "sharded", Pal.accent.cpy(),
-            Color.valueOf("ffd37f"), Color.valueOf("eab678"), Color.valueOf("d4816b")),
-        crux = new Team(2, "crux", Color.valueOf("f25555"),
-            Color.valueOf("fc8e6c"), Color.valueOf("f25555"), Color.valueOf("a04553")),
-        green = new Team(3, "green", Color.valueOf("54d67d"), Color.valueOf("96f58c"), Color.valueOf("54d67d"), Color.valueOf("28785c")),
-        purple = new Team(4, "purple", Color.valueOf("995bb0"), Color.valueOf("f08dd5"), Color.valueOf("995bb0"), Color.valueOf("312c63")),
-        blue = new Team(5, "blue", Color.valueOf("554deb"), Color.valueOf("80aaff"), Color.valueOf("554deb"), Color.valueOf("3f207d"));
+        //derelict = new Team(0, "derelict", Color.valueOf("4d4e58")),
+        derelict = new TeamBuilder()
+                .setId(0)
+                .setName("derelict")
+                .setColor(Color.valueOf("4d4e58"))
+                .initPalette()
+                .build(),
+        sharded = new TeamBuilder()
+                .setId(1)
+                .setName("sharded")
+                .setColor(Pal.accent.cpy())
+                .initPalette()
+                .setPalette(Color.valueOf("ffd37f"), Color.valueOf("eab678"), Color.valueOf("d4816b"))
+                .build(),
+        crux = new TeamBuilder()
+                .setId(2)
+                .setName("crux")
+                .setColor(Color.valueOf("f25555"))
+                .initPalette()
+                .setPalette(Color.valueOf("fc8e6c"), Color.valueOf("f25555"), Color.valueOf("a04553"))
+                .build(),
+        green = new TeamBuilder()
+                .setId(3)
+                .setName("green")
+                .setColor(Color.valueOf("54d67d"))
+                .initPalette()
+                .setPalette(Color.valueOf("96f58c"), Color.valueOf("54d67d"), Color.valueOf("28785c"))
+                .build(),
+        purple = new TeamBuilder()
+                .setId(4)
+                .setName("purple")
+                .setColor(Color.valueOf("995bb0"))
+                .initPalette()
+                .setPalette(Color.valueOf("f08dd5"), Color.valueOf("995bb0"), Color.valueOf("312c63"))
+                .build(),
+        blue = new TeamBuilder()
+                .setId(5)
+                .setName("blue")
+                .setColor(Color.valueOf("554deb"))
+                .initPalette()
+                .setPalette(Color.valueOf("80aaff"), Color.valueOf("554deb"), Color.valueOf("3f207d"))
+                .build();
 
     static{
         Mathf.rand.setSeed(8);
         //create the whole 256 placeholder teams
         for(int i = 6; i < all.length; i++){
-            new Team(i, "team#" + i, Color.HSVtoRGB(360f * Mathf.random(), 100f * Mathf.random(0.6f, 1f), 100f * Mathf.random(0.8f, 1f), 1f));
+            new TeamBuilder()
+                    .setId(i)
+                    .setName("team#" + i)
+                    .setColor(Color.HSVtoRGB(360f * Mathf.random(), 100f * Mathf.random(0.6f, 1f), 100f * Mathf.random(0.8f, 1f), 1f))
+                    .initPalette()
+                    .build();
         }
         Mathf.rand.setSeed(new Rand().nextLong());
     }
@@ -50,35 +89,76 @@ public class Team implements Comparable<Team>{
         return all[((byte)id) & 0xff];
     }
 
-    protected Team(int id, String name, Color color){
-        this.name = name;
-        this.color = color;
-        this.id = id;
+    protected static class TeamBuilder{
+        public int id;
+        public Color color;
+        public Color[] palette;
+        public int[] palettei = new int[3];
+        public boolean hasPalette;
+        public String name;
 
-        if(id < 6) baseTeams[id] = this;
-        all[id] = this;
+        protected TeamBuilder(){}
 
-        palette = new Color[3];
-        palette[0] = color;
-        palette[1] = color.cpy().mul(0.75f);
-        palette[2] = color.cpy().mul(0.5f);
+        protected TeamBuilder setId(int id){
+            this.id = id;
+            return this;
+        }
 
-        for(int i = 0; i < 3; i++){
-            palettei[i] = palette[i].rgba();
+        protected TeamBuilder setName(String name){
+            this.name = name;
+            return this;
+        }
+
+        protected TeamBuilder setColor(Color color){
+            this.color = color;
+            return this;
+        }
+
+        protected TeamBuilder initPalette(){
+            palette = new Color[3];
+            palette[0] = color;
+            palette[1] = color.cpy().mul(0.75f);
+            palette[2] = color.cpy().mul(0.5f);
+
+            for(int i = 0; i < 3; i++){
+                palettei[i] = palette[i].rgba();
+            }
+            return this;
+        }
+
+        protected TeamBuilder setPalette(Color pal1, Color pal2, Color pal3){
+            palette[0] = pal1;
+            palette[1] = pal2;
+            palette[2] = pal3;
+            for(int i = 0; i < 3; i++){
+                palettei[i] = palette[i].rgba();
+            }
+            hasPalette = true;
+
+            return this;
+        }
+
+        protected Team build(){
+            return new Team(this);
         }
     }
 
-    /** Specifies a 3-color team palette. */
-    protected Team(int id, String name, Color color, Color pal1, Color pal2, Color pal3){
-        this(id, name, color);
+    protected Team(TeamBuilder teamBuilder){
+        this.name = teamBuilder.name;
+        this.color = teamBuilder.color;
+        this.id = teamBuilder.id;
 
-        palette[0] = pal1;
-        palette[1] = pal2;
-        palette[2] = pal3;
+        if(teamBuilder.id < 6) baseTeams[teamBuilder.id] = this;
+        all[teamBuilder.id] = this;
+
+        palette = new Color[3];
+
         for(int i = 0; i < 3; i++){
-            palettei[i] = palette[i].rgba();
+            palette[i] = teamBuilder.palette[i];
+            palettei[i] = teamBuilder.palettei[i];
         }
-        hasPalette = true;
+
+        hasPalette = teamBuilder.hasPalette;
     }
 
     /** @return the core items for this team, or an empty item module.
